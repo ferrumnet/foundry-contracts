775aae7798a8890d79b71b83507e5f084f3675fd
Naiem Yeganeh <naiem@ferrum.network>
2022-11-15 21:52:26 +0200

    Initial Migration from Private Repository

---
+++ contracts/token/DaoMintableErc20.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.2;
+import "../common/IFerrumDeployer.sol";
+import "../signature/IDaoCallable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract DaoMintableErc20 is Ownable, ERC20Burnable, IDaoCallable {
+	string public _name;
+	string public _symbol;
+	constructor() ERC20("", "") {
+		(_name, _symbol) = abi.decode(IFerrumDeployer(msg.sender).initData(), (string, string));
+	}
+
+    /**
+     @notice The toke name
+     */
+	function name() public override view returns (string memory) {
+		return _name;
+	}
+
+    /**
+     @notice The toke symbol
+     */
+	function symbol() public override view returns (string memory) {
+		return _symbol;
+	}
+
+    bytes32 constant TOKEN_MINT_METHOD =
+        keccak256(
+            "Mint(uint256 amount,address to)"
+        );
+    /**
+     @notice Runs an action requested by DAO. Note that owner should be a DAO contract
+     @param action The action
+     @param parameters The action parameters
+     */
+    function daoAction(bytes32 action, bytes calldata parameters) external override onlyOwner {
+        require(action == TOKEN_MINT_METHOD, "TD: bad action");
+		(address to, uint256 supply) = abi.decode(parameters, (address, uint256));
+		_mint(to, supply);
+    }
+}
---
+++ contracts/token/MiniErc20.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <=0.8.2;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "../common/IFerrumDeployer.sol";
+
+contract MinimalErc20 is ERC20Burnable {
+	string public _name;
+	string public _symbol;
+	constructor() ERC20("", "") {
+		uint256 supply;
+		address to;
+		(_name, _symbol, to, supply) = abi.decode(IFerrumDeployer(msg.sender).initData(), (string, string, address, uint256));
+		_mint(to, supply);
+	}
+
+	function name() public override view returns (string memory) {
+		return _name;
+	}
+
+	function symbol() public override view returns (string memory) {
+		return _symbol;
+	}
+}
+
+contract MinimalErc20Deployer is IFerrumDeployer {
+	bytes public override initData;
+	event TokenDeployed(address token);
+
+	function deploy(
+		string memory name,
+		string memory symbol,
+		address to,
+		uint256 totalSupply
+	) external {
+		(bytes32 salt, bytes memory data) = saltAndData(name, symbol, to, totalSupply, msg.sender);
+		initData = data;
+		address token = address(new MinimalErc20{ salt: salt }());
+		delete initData;
+		emit TokenDeployed(token);
+	}
+
+	function getsaltAndData(
+		string memory name,
+		string memory symbol,
+		address to,
+		uint256 totalSupply,
+		address msgSender
+	) external pure returns (bytes32 salt, bytes memory data) {
+		(salt, data) = saltAndData(name, symbol, to, totalSupply, msgSender);
+	}
+
+	function saltAndData(
+		string memory name,
+		string memory symbol,
+		address to,
+		uint256 totalSupply,
+		address msgSender
+	) internal pure returns (bytes32 salt, bytes memory data) {
+		salt = keccak256(abi.encode(msgSender, name, symbol));
+		data = abi.encode(name, symbol, to, totalSupply);
+	}
+}
